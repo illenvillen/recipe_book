81f36e6307da9fadcb5bc3186e2db4783f358004
Chris Detlef <cdetlef23@gmail.com>
2019-03-26 14:25:09 -0400

    Trim off blank steps and ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -31,7 +31,24 @@ class RecipesController < ApplicationController
       @recipe.save!
 
       @recipe.steps.each do |step|
-        step.save!
+        unless step.step_text.empty?
+          step.save!
+        else
+          step.destroy!
+        end
+      end
+
+      @recipe.ingredient_amounts do |ia|
+        ap "///////////////////////////////////////////////////////"
+        ap "Ingredient!!!!!!!!!"
+        ap "///////////////////////////////////////////////////////"
+        ap ia
+        ap ia.ingredient
+        unless ia.ingredient.name.empty?
+          ia.save!
+        else
+          ia.destroy!
+        end
       end
 
       redirect_to recipe_path(@recipe)
@@ -45,13 +62,34 @@ class RecipesController < ApplicationController
   end
 
   def update
-    params[:recipe][:course_types] = CourseType.where(:id => params[:recipe][:course_types].to_i)
-    params[:recipe][:cuisine_categories] = CuisineCategory.where(:id => params[:recipe][:cuisine_categories].to_i)
+    params[:recipe][:course_types] = map_course_types
+    params[:recipe][:cuisine_categories] = map_cuisine_categories
     
     @recipe.update_attributes(params[:recipe].to_unsafe_hash)
 
-    @recipe.steps.each do |step|
-      step.save!
+    ap @recipe
+    ap @recipe.ingredient_amounts.count
+    ap @recipe.ingredient_amounts.map { |ia| ia.ingredient }
+    ap @recipe.ingredient_amounts.map { |ia| ia.ingredient.name.empty? }
+
+    ActiveRecord::Base.transaction do
+      @recipe.steps.each do |step|
+        unless step.step_text.empty?
+          step.save!
+        else
+          step.destroy!
+        end
+      end
+    end
+
+    ActiveRecord::Base.transaction do
+      @recipe.ingredient_amounts.to_a.each do |ia|
+        if ia.ingredient.name.empty?
+          ia.destroy! 
+        else
+          ia.save!
+        end
+      end
     end
 
     redirect_to recipe_path(@recipe)
@@ -61,5 +99,13 @@ class RecipesController < ApplicationController
 
   def load_recipe
     @recipe = Recipe.find(params[:id])
+  end
+
+  def map_course_types
+    CourseType.where(:id => params[:recipe][:course_types].map { |ct| ct.to_i })
+  end
+
+  def map_cuisine_categories
+    CuisineCategory.where(:id => params[:recipe][:cuisine_categories].map { |cc| cc.to_i })
   end
 end
--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,3 +1,20 @@
 class Amount < ApplicationRecord
   has_many :ingredient_amounts
+
+  MEASUREMENTS = [
+    ["", 0],
+    ["Teaspoons", 1],
+    ["Tablespoons", 2],
+    ["Fliod Ounces", 3],
+    ["Cups", 4],
+    ["Pints", 5],
+    ["Quarts", 6],
+    ["Gallons", 7],
+    ["Milliliters", 8],
+    ["Liters", 9],
+    ["Ounces", 10],
+    ["Pounds", 11],
+    ["Grams", 12],
+    ["Kilograms", 13]
+  ]
 end
